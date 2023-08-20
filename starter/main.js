ea8f341f16c48d8ab3b3e5089247528143515070
DamilolaOladipupo <akinsoladamilola29@gmail.com>
2023-08-20 14:42:00 -0700

    modified project for better optimisation

---
+++ starter/main.js
@@ -0,0 +1,27 @@
+ScrollReveal({
+  reset: true,
+  distance: "100px",
+  origin: "left",
+  duration: 2000,
+});
+ScrollReveal().reveal(".logotext", {
+  origin: "right",
+  scale: 3,
+  distance: "800px",
+});
+ScrollReveal().reveal(".heading");
+ScrollReveal().reveal(".firstsection-leftdiv p", { origin: "right" });
+ScrollReveal().reveal(".line", {
+  origin: "bottom",
+  delay: 1000,
+});
+ScrollReveal().reveal(".secondsection-leftdiv", {
+  rotate: {
+    y: 180,
+  },
+});
+ScrollReveal().reveal(".thirdsection-rightdiv", {
+  rotate: {
+    y: 180,
+  },
+});
